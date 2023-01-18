b36db7264e91d8e4817e96323a09d1ba1958a3b4
Natalia Zvereva <xx029xx@ya.ru>
2023-01-18 10:08:21 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 2000;
+        int miles = price / 20;
+
+        System.out.println("Количество начисленных миль за покупку билета " + miles);
+    }
+}
